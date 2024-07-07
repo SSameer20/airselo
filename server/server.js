593119baf2609dccab0fa0c141f890f191913e0d
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-07-08 02:09:09 +0530

    setup: project setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const {meal , label} = require("./data")
+
+// app
+const app = express();
+
+/// middle ware
+app.use(cors());
+// Routes
+app.get("/", (req, res) => {
+    res.status(201).send({msg : "Server is running"})
+})
+
+app.get("/data", (req, res) => {
+    res.status(201).send({"meal" : meal, "label" : label})
+})
+
+
+
+
+/// listen
+app.listen(3001, (req, res) => {
+    console.log(`The server is running on http://localhost:3001`)
+})
