47625a94555d454478aad67bb8d4b6a6360ce96e
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-07-08 02:10:49 +0530

    fix

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import "../styles/menu.css"
+import Vine from "../media/Vine.jpg";
+import Beer from "../media/Beer.jpg";
+import Juice from "../media/Juice.jpg";
+
+
+export default function Menu() {
+  const [label, setLabel] = useState(
+[]
+  );
+  const [data, setData] = useState([])
+  const [meal, setMeal] = useState([]);
+  const [selectDrink, setSelectDrink] = useState(false)
+
+
+  const fetchHandler = async () => {
+    await axios.get("http://localhost:3001/data")
+      .then((res) => {
+        setData(res.data.meal)
+        setLabel(res.data.label);
+        setMeal(res.data.meal)
+      })
+      .catch((err) => alert("Error While Connecting to Server " + err))
+  }
+
+  //  Calling the Fetch
+
+  useEffect(() => {
+    fetchHandler();
+  }, [])
+
+
+  // Filter 
+
+  const handleFilter = (e) => {
+    const item = e.target.value;
+    if(item !== "") {
+      setMeal(data)
+      const filter = data.filter((food) => food.labels.includes(item));
+      setMeal(filter)
+    }
+    else {
+      setMeal(data);
+    }
+  }
+
+const handleDrink = (e) => {
+  console.log(e.target.getAttribute('value'));
+}
+
+
+  return (
+    <div className="menu">
+      <div className="menu-area">
+        <div className="tags">
+        <button onClick={handleFilter} value="">ALL</button>
+          {
+            label.map((item) => {
+              return <button onClick={handleFilter} value={item.id}>{item.label}</button>
+
+            })
+
+          }
+        </div>
+        <div className="order-cards">
+          {
+            meal.map((item) => {
+              return <div className="card">
+                <div className="card-image">
+                <img src={item.img} alt={item.id} className='card-img' onError={(e) => { e.target.src = 'https://via.placeholder.com/150'; }} />
+                </div>
+                <div className="details">
+                  <span className='meal-details'>{item.title}</span>
+                  <div className="food-details">
+                    <span><strong>Starter: </strong>{item.starter}</span>
+                    <span><strong>Desert: </strong>{item.desert}</span>
+                    {selectDrink ? (<span><strong>Selected drink: </strong> Orange Juice</span>) : (<span></span>)}
+                  </div>
+                  <div className="card-bottom">
+                    <div className="drinks">
+                     <div className='drink'>
+                      {
+                        item.drinks.map((drink) => {
+                          return <img 
+                          src={drink.title === "Vine" ? Vine : drink.title === "Juice" ? Juice : Beer} 
+                          alt={drink.title} 
+                          value={drink.price}
+                          onClick={handleDrink}/>
+                        })
+
+                      }
+                      
+
+                     </div>
+                    </div>
+                    <div className="price">
+                      <span>{item.price}</span>
+                      <button>Select</button>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            })
+          }
+
+          
+
+
+
+        </div>
+
+      </div>
+      <div className="menu-order">
+
+      </div>
+    </div>
+  )
+}
---
+++ server/data.js
@@ -0,0 +1,233 @@
+const label =  [
+      {
+        "id": "pork",
+        "label": "Pork"
+      },
+      {
+        "id": "seafood",
+        "label": "Seafood"
+      },
+      {
+        "id": "kids",
+        "label": "Kids"
+      },
+      {
+        "id": "chicken",
+        "label": "Chicken"
+      },
+      {
+        "id": "beef",
+        "label": "Beef"
+      },
+      {
+        "id": "vegetarian",
+        "label": "Vegetarian"
+      },
+      {
+        "id": "breakfast",
+        "label": "Breakfast"
+      }
+    ]
+
+const meal =  [
+      {
+        "id": "meal1",
+        "title": "3 course chicken",
+        "starter": "Lorem Ipsum",
+        "desert": "Cake",
+        "price": 9.99,
+        "labels": [
+          "chicken",
+          "breakfast"
+        ],
+        "img": "https://static.independent.co.uk/2022/05/02/11/iStock-1044323290.jpg",
+        "drinks": [
+          {
+            "id": "drink-1",
+            "title": "Vine",
+            "price": 4.99
+          },
+          {
+            "id": "drink-2",
+            "title": "Juice",
+            "price": 5.99
+          },
+          {
+            "id": "drink-3",
+            "title": "Beer",
+            "price": 6.99
+          }
+        ]
+      },
+      {
+        "id": "meal2",
+        "title": "3 course Beef",
+        "starter": "Lorem Ipsum",
+        "desert": "Cake",
+        "price": 19.99,
+        "labels": [
+          "beef"
+        ],
+        "img": "https://www.australianeggs.org.au/assets/blog-posts/211022-Blog-Post-Header-Image-3-Course-Dinner-Party-Ideas__ScaleWidthWzEyMDBd.jpg",
+        "drinks": [
+          {
+            "id": "drink-1",
+            "title": "Vine",
+            "price": 4.99
+          },
+          {
+            "id": "drink-2",
+            "title": "Juice",
+            "price": 5.99
+          },
+          {
+            "id": "drink-3",
+            "title": "Beer",
+            "price": 6.99
+          }
+        ]
+      },
+      {
+        "id": "meal3",
+        "title": "3 course Vegetarian",
+        "starter": "Lorem Ipsum",
+        "desert": "Cake",
+        "price": 79.99,
+        "labels": [
+          "vegetarian"
+        ],
+        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU9GIpujRfpC8HWO4TPdlH0OA9SDCc2YxEPsPdh0N_YkIzXJh5AUHA3i0_RnJcBSttzaU&usqp=CAU",
+        "drinks": [
+          {
+            "id": "drink-1",
+            "title": "Vine",
+            "price": 4.99
+          },
+          {
+            "id": "drink-2",
+            "title": "Juice",
+            "price": 5.99
+          },
+          {
+            "id": "drink-3",
+            "title": "Beer",
+            "price": 6.99
+          }
+        ]
+      },
+      {
+        "id": "meal4",
+        "title": "3 course Seafood",
+        "starter": "Lorem Ipsum",
+        "desert": "Cake",
+        "price": 49.99,
+        "labels": [
+          "seafood"
+        ],
+        "img": "https://www.foodandwine.com/thmb/ClPnka2WSnl5PtrMYOjlmXsXw1k=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/escovitch-fish-FT-RECIPE0920-8a733638c2ba4b72b48737782fa616c2.jpg",
+        "drinks": [
+          {
+            "id": "drink-1",
+            "title": "Vine",
+            "price": 4.99
+          },
+          {
+            "id": "drink-2",
+            "title": "Juice",
+            "price": 5.99
+          },
+          {
+            "id": "drink-3",
+            "title": "Beer",
+            "price": 6.99
+          }
+        ]
+      },
+      {
+        "id": "meal5",
+        "title": "3 course Pork",
+        "starter": "Lorem Ipsum",
+        "desert": "Cake",
+        "price": 39.99,
+        "labels": [
+          "pork"
+        ],
+        "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX3vJZR5sZ1O6eOqEyyJBEWGbe-rTt2mcv-w&s",
+        "drinks": [
+          {
+            "id": "drink-1",
+            "title": "Vine",
+            "price": 4.99
+          },
+          {
+            "id": "drink-2",
+            "title": "Juice",
+            "price": 5.99
+          },
+          {
+            "id": "drink-3",
+            "title": "Beer",
+            "price": 6.99
+          }
+        ]
+      },
+      {
+        "id": "meal6",
+        "title": "3 course Kids",
+        "starter": "Lorem Ipsum",
+        "desert": "Cake",
+        "price": 29.99,
+        "labels": [
+          "kids",
+          "breakfast"
+        ],
+        "img": "https://qph.cf2.quoracdn.net/main-qimg-c444274bc11709f89001de706002aaf7",
+        "drinks": [
+          {
+            "id": "drink-1",
+            "title": "Vine",
+            "price": 4.99
+          },
+          {
+            "id": "drink-2",
+            "title": "Juice",
+            "price": 5.99
+          },
+          {
+            "id": "drink-3",
+            "title": "Beer",
+            "price": 6.99
+          }
+        ]
+      },
+      {
+        "id": "meal7",
+        "title": "3 course Chicken 2",
+        "starter": "Lorem Ipsum",
+        "desert": "Cake",
+        "price": 19.99,
+        "labels": [
+          "chicken"
+        ],
+        "img": "https://pataksusa.com/wp-content/uploads/2023/09/3-step-butter-chicken.jpg",
+        "drinks": [
+          {
+            "id": "drink-1",
+            "title": "Vine",
+            "price": 4.99
+          },
+          {
+            "id": "drink-2",
+            "title": "Juice",
+            "price": 5.99
+          },
+          {
+            "id": "drink-3",
+            "title": "Beer",
+            "price": 6.99
+          }
+        ]
+      }
+    ]
+
+  module.exports = {meal, label};
