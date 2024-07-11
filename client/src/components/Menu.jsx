e9e8fca62eee5d9104ff0d2afa42aa15e0730d5c
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-07-11 18:00:17 +0530

    doc : read me and assets fix

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -11,7 +11,11 @@ export default function Menu() {
   const [label, setLabel] = useState([]);
   const [data, setData] = useState([])
   const [meal, setMeal] = useState([]);
+  const [price, setPrice] = useState(0);
   const [selectDrink, setSelectDrink] = useState(false)
+  const [order, setOrder] = useState([
+    
+  ])
 
 
   const fetchHandler = async () => {
@@ -26,16 +30,9 @@ export default function Menu() {
 
   //  Calling the Fetch
 
-  useEffect(() => {
-    fetchHandler();
-  }, [])
-
-
-  // Filter 
-
   const handleFilter = (e) => {
     const item = e.target.value;
-    if(item !== "") {
+    if (item !== "") {
       setMeal(data)
       const filter = data.filter((food) => food.labels.includes(item));
       setMeal(filter)
@@ -44,87 +41,130 @@ export default function Menu() {
       setMeal(data);
     }
   }
+  useEffect(() => {
+    fetchHandler();
+  }, [handleFilter])
 
-const handleDrink = (e) => {
-  console.log(e.target.getAttribute('value'));
 
-}
+  // Filter 
 
-const addItem = (cost) => {
-  // console.log(cost);
-  
-}
+
+  const handleDrink = (e) => {
+    console.log(e.target.getAttribute('value'));
+
+  }
+
+  const addItem = (item) => {
+    console.log(item)
+    const newItem = { id: item.id, price: Math.round(item.price * 100) / 100 }
+    setOrder([...order, newItem])
+    setPrice( price +  Math.round(item.price * 100) / 100 )
+  }
+
+  const deleteItem = (item) => {
+    setOrder(order.filter((menu) => menu.id !== item.id))
+    const newPrice = Math.max(price - Math.round(item.price * 100) / 100, 0); 
+    try {
+      setPrice(newPrice);
+    } catch (error) {
+      setPrice(0);
+      
+    }
+    
+  }
 
 
   return (
     <>
       <Navigation />
-    <div className="menu">
-      <div className="menu-area">
-        <div className="tags">
-        <button onClick={handleFilter} value="">ALL</button>
-          {
-            label.map((item) => {
-              return <button onClick={handleFilter} value={item.id}>{item.label}</button>
-              
-            })
+      <div className="menu">
+        <div className="menu-area">
+          <div className="tags">
+            <button onClick={handleFilter} value="">ALL</button>
+            {
+              label.map((item) => {
+                return <button onClick={handleFilter} value={item.id}>{item.label}</button>
 
-          }
-        </div>
-        <div className="order-cards">
-          {
-            meal.map((item) => {
-              return <div className="card">
-                <div className="card-image">
-                <img src={item.img} alt={item.id} className='card-img' onError={(e) => { e.target.src = 'https://via.placeholder.com/150'; }} />
-                </div>
-                <div className="details">
-                  <span className='meal-details'>{item.title}</span>
-                  <div className="food-details">
-                    <span><strong>Starter: </strong>{item.starter}</span>
-                    <span><strong>Desert: </strong>{item.desert}</span>
-                    {selectDrink ? (<span><strong>Selected drink: </strong> Orange Juice</span>) : (<span></span>)}
+              })
+
+            }
+          </div>
+          <div className="order-cards">
+            {
+              meal.map((item) => {
+                return <div className="card">
+                  <div className="card-image">
+                    <img src={item.img} alt={item.id} className='card-img' onError={(e) => { e.target.src = 'https://via.placeholder.com/150'; }} />
                   </div>
-                  <div className="card-bottom">
-                    <div className="drinks">
-                     <div className='drink'>
-                      {
-                        item.drinks.map((drink) => {
-                          return <img 
-                          src={drink.title === "Vine" ? Vine : drink.title === "Juice" ? Juice : Beer} 
-                          alt={drink.title} 
-                          value={drink.price}
-                          flag="false"
-                          onClick={handleDrink}/>
-                        })
-
-                      }
-                      
-
-                     </div>
+                  <div className="details">
+                    <span className='meal-details'>{item.title}</span>
+                    <div className="food-details">
+                      <span><strong>Starter: </strong>{item.starter}</span>
+                      <span><strong>Desert: </strong>{item.desert}</span>
+                      {selectDrink ? (<span><strong>Selected drink: </strong> Orange Juice</span>) : (<span></span>)}
                     </div>
-                    <div className="price">
-                      <span>{item.price}</span>
-                      <button onClick={addItem(item.price)}>Select</button>
+                    <div className="card-bottom">
+                      <div className="drinks">
+                        <div className='drink'>
+                          {
+                            item.drinks.map((drink) => {
+                              return <img
+                                src={drink.title === "Vine" ? Vine : drink.title === "Juice" ? Juice : Beer}
+                                alt={drink.title}
+                                value={drink.price}
+                                onClick={handleDrink} />
+                            })
+
+                          }
+
+
+                        </div>
+                      </div>
+                      <div className="price">
+                        <span>{item.price}</span>
+                        <button onClick={() => addItem(item)}>Select</button>
+                      </div>
                     </div>
                   </div>
+
                 </div>
-
-              </div>
-            })
-          }
-
-          
+              })
+            }
 
 
+
+
+
+          </div>
 
         </div>
+        <div className="menu-order">
+          <p>Order Details</p>
+          <details className='bill-items'>
+            <summary>Items Ordered</summary>
+            {order.map(item => (
+              <span key={item.id}>
+                {item.id} - $ {item.price}
+                <button onClick={() => deleteItem(item)}>x</button>
+              </span>
+            ))}
 
-      </div>
-      <div className="menu-order">
+          </details>
 
+          <details className='bill-drinks'>
+            <summary>Drinks Ordered</summary>
+            {order.map(item => (
+              <span key={item.id}>
+                {item.id} -  {item.price} $
+                <button onClick={() => deleteItem(item.id)}>Delete</button>
+              </span>
+            ))}
+
+          </details>
+          <p>Total Bill : {price}</p>
+
+        </div>
       </div>
-    </div>
-</>
+    </>
   )
 }
